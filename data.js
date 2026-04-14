612545a485023b54860b8e3580e4612c7f286a29
Leonardo Maltese <leonardomaltese@MacBookPro.Home>
2026-04-14 20:39:08 +0200

    Update

--- data.js
+++ data.js
@@ -68,7 +68,7 @@ const PROJECTS = [
       href: 'https://leowl-M.github.io/training-dithering/',
       addedAt: '2025-04-02T08:00:00Z',
       status: 'stable',
-      version: '1.0',
+      version: '2.0',
     },
     {
       id: 'articleimage',
